465e0e1a4f7c0b910a1a770d971a4d0d8441b2a8
nicolaceccagnoli <nicola.ceccagnoli.94@gmail.com>
2024-01-25 15:32:46 +0100

    Edit Search Input

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,10 +4,14 @@ import { store } from '../store';
     export default {
         data() {
             return {
-                store
+                store,
+                search: false
             };
         },
         methods: {
+            showSearchBar(){
+                this.search = true;
+            }
         },
         components: {
         },
@@ -24,17 +28,24 @@ import { store } from '../store';
             </div>
 
             <div class="col-auto">
+
                 <form @submit.prevent="$emit('performSearch')" class="input-group my-3">
-                    <!-- <div>
-                        {{ store.searchFilm }}
-                    </div> -->
-                    <input 
+                    <button @click="showSearchBar()" 
+                    v-show="search == false"
+                    class="btn btn-outline-secondary text-white">
+                        <i class="fa-solid fa-magnifying-glass"></i>
+                    </button>
+                    <input
+                    v-show="search == true"
                     v-model="store.searchFilm"
                     type="text" placeholder="Search Film" aria-label="Search Film" aria-describedby="button-addon2">
-                    <button class="btn btn-outline-secondary" type="submit" id="button-addon2">
+                    <button 
+                    v-show="search == true"
+                    class="text-white btn btn-outline-secondary" type="submit" id="button-addon2">
                         Search
                     </button>
                 </form>
+
             </div>
 
         </div>
@@ -51,5 +62,6 @@ header {
     #logo img {
         max-width: 100px;
     }
+
 }
 </style>
